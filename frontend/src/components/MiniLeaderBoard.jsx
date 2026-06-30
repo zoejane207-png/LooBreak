9f9f20dbd84b0cbaa489a0f2563859f1ff6c18f2
Zoe <zoejane207@gmail.com>
2026-06-30 14:23:36 +0100

    Added mini leaderboard to homepage

---
+++ frontend/src/components/MiniLeaderBoard.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import { getPlayers } from "../services/results";
+
+export default function MiniLeaderboard() {
+  const [players, setPlayers] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getPlayers()
+      .then((data) => {
+        setPlayers(data.players.slice(0, 3));
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [players]);
+
+  return (
+    <>
+      <div data-testId="mini-leaderboard">
+        <table className="table-auto">
+          <thead>
+            <tr>
+              <th>Playername</th>
+              <th>Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            {loading ? (
+              <p>Loading...</p>
+            ) : (
+              players.map((player) => (
+                <tr key={player.index}>
+                  <td>{player.playername}</td>
+                  <td>{player.score}/10</td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
