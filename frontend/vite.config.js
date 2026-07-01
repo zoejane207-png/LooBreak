e69f962ab88302b2a14a93fa9cd14fb60960354a
Kelly-Huynh <96kellyhuynh@gmail.com>
2026-07-01 11:29:50 +0100

    shadcn setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "./src"),
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
