b2d53b422b4df9ffdbaa27b3e6d1dcff7bd1c7b8
samflynn98 <sam.flynn98@outlook.com>
2026-07-01 09:44:59 +0100

    sams quiz

--- api/services/externalQuizApi.js
+++ api/services/externalQuizApi.js
@@ -1,8 +1,8 @@
-async function fetchExternalQuiz() {
-  const res = await fetch(
-      //"http://localhost:8000/data"
-      "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple",
-  );
+ async function fetchExternalQuiz() {
+    const res = await fetch(
+        "http://localhost:8000/data"
+        //"https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple",
+    );
   if (!res.ok) throw new Error(`Response status: ${res.status}`);
   return res.json();
 }
